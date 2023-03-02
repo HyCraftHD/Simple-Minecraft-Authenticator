c25dfdb652117cb04074c116f9621845b047741b
HyCraftHD <hycrafthd@live.de>
2023-03-02 18:43:34 +0100

    Start modularizing project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module net.hycrafthd.simple_minecraft_authenticator {
+	
+	exports net.hycrafthd.simple_minecraft_authenticator;
+	
+	requires jopt.simple;
+	requires transitive net.hycrafthd.minecraft_authenticator;
+}
