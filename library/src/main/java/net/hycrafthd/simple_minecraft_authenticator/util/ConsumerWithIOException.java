a6fe70b3a29d79d755cbb990b8f2b5005ea3049d
HyCraftHD <hycrafthd@live.de>
2023-04-17 14:00:05 +0200

    Remove unused interfaces

--- library/src/main/java/net/hycrafthd/simple_minecraft_authenticator/util/ConsumerWithIOException.java
+++
@@ -1,9 +0,0 @@
-package net.hycrafthd.simple_minecraft_authenticator.util;
-
-import java.io.IOException;
-
-@FunctionalInterface
-public interface ConsumerWithIOException<T> {
-	
-	void accept(T t) throws IOException;
-}
--- library/src/main/java/net/hycrafthd/simple_minecraft_authenticator/util/FunctionWithIOException.java
+++
@@ -1,9 +0,0 @@
-package net.hycrafthd.simple_minecraft_authenticator.util;
-
-import java.io.IOException;
-
-@FunctionalInterface
-public interface FunctionWithIOException<T, R> {
-	
-	R apply(T t) throws IOException;
-}
